7ee2987429047aaf38d53c2f87ddf5afdc44a5a7
wpbindt <wesselbindt@gmail.com>
2021-03-10 20:19:53 +0100

    Fix circular imports

--- src/mutator.py
+++ src/mutator.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Generic
 
-from .genetic_algorithm import Candidate
+from .candidate_type import Candidate
 
 
 class Mutator(ABC, Generic[Candidate]):
