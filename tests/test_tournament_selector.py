e7bd82ed3d48f4a3ab8eea1b5163ff6f1c3be4d6
wpbindt <wesselbindt@gmail.com>
2021-03-10 18:36:56 +0100

    More tournament select test cases

--- tests/test_tournament_selector.py
+++ tests/test_tournament_selector.py
@@ -6,4 +6,11 @@ def test_tournament_select():
     assert selector.select([], lambda x: x) == []
     assert selector.select([1], lambda x: x) == [1]
     assert selector.select([1, 2], lambda x: x) == [2, 2]
+    assert selector.select([1, 2, 2], lambda x: x) == [2, 2, 2]
     assert selector.select([1, 2], lambda x: -x) == [1, 1]
+
+
+def test_population_size_invariant():
+    selector = TournamentSelector()
+    population = list(range(100))
+    assert len(selector.select(population, lambda x: x)) == len(population)
